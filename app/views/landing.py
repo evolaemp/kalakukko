886d6845cd369699cce13f28c00c5b822deac71e
pavelsof <pavelsof@gmail.com>
2018-04-04 16:53:34 +0200

    Updated most pip dependencies.

--- app/views/landing.py
+++ app/views/landing.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import View
@@ -9,34 +8,28 @@ from app.models import Language
 from utils.json import make_json
 
 
+
 class LandingView(View):
+
 	@method_decorator(ensure_csrf_cookie)
 	def dispatch(self, *args, **kwargs):
 		"""
 		Ensures sending of csrf cookie.
 		"""
 		return super(LandingView, self).dispatch(*args, **kwargs)
-	
+
+
 	def get(self, request):
 		"""
 		Renders the landing page.
 		"""
 		languages = Language.objects.filter(
 			latitude__isnull = False,
-			longitude__isnull = False
-		)
-		
-		return render_to_response(
-			'landing.html',
-			{
-				'languages': make_json(
-					[language.to_dict() for language in languages]
-				),
-				'OSM_ACCESS_TOKEN': settings.OSM_ACCESS_TOKEN,
-				'OSM_ID': settings.OSM_ID
-			},
-			context_instance = RequestContext(request)
-		)
+			longitude__isnull = False)
 
+		context = {
+			'languages': make_json([lang.to_dict() for lang in languages]),
+			'OSM_ACCESS_TOKEN': settings.OSM_ACCESS_TOKEN,
+			'OSM_ID': settings.OSM_ID }
 
-
+		return render(request, 'landing.html', context)
